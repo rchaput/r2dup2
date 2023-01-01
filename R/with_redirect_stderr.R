47af544d0bc314d8c48afca58001d71f9e37131e
rchaput <rchaput.pro@gmail.com>
2023-01-01 13:07:13 +0100

    Added ability to capture stderr in a variable

--- R/with_redirect_stderr.R
+++ R/with_redirect_stderr.R
@@ -6,7 +6,8 @@
 #' the R level and deals with R objects, this method truly redirects the
 #' stream at a low (system) level.
 #' Thus, the redirection works for low-level system calls, including
-#' external processes invoked through [base::system()].
+#' external processes invoked through [base::system()], [base::system2()],
+#' or similar.
 #'
 #' @details
 #' # Known problems
@@ -16,12 +17,17 @@
 #' In this case, try using the R Console directly from a terminal.
 #'
 #' @param expression The code to evaluate while redirecting stderr.
-#' @param file The file to which all text will be redirected.
+#' @param file The file to which all text will be redirected. By default,
+#'   (`NULL`), a temporary file is automatically created, and the captured
+#'   text is returned by the function.
 #' @param append Whether to append to the file, when it already exists.
-#'   By default (false), the file is truncated. If the file does not exist,
-#'   it is created.
+#'   By default (`FALSE`), the file is truncated. If the file does not exist,
+#'   it is created. This argument has no effect when `file` is `NULL`.
 #'
-#' @return NULL
+#' @return The return value depends on the `file` parameter:
+#'   * When `file` is `NULL`, the function returns the captured text,
+#'     as returned by [base::readLines()].
+#'   * Otherwise, the function does not return anything (an invisible `NULL`).
 #'
 #' @examples
 #' \dontrun{
@@ -32,13 +38,29 @@
 #' with_redirect_stderr(file = "test.err", append = TRUE, {
 #'   system("echo This will not truncate the previous line! >&2")
 #' })
+#'
+#' stderr <- with_redirect_stderr({
+#'   system("echo Redirecting to a variable also works! >&2")
+#' })
 #' }
 #'
 #' @export
 #'
-with_redirect_stderr <- function (expression, file, append = FALSE) {
+with_redirect_stderr <- function (expression, file = NULL, append = FALSE) {
+
+  # If the user did not specify a file, create a temporary one
+  if (is.null(file)) {
+    filepath <- tempfile(fileext = ".err")
+    if (append) {
+      # Using `append` is irrelevant, since we create a temporary file
+      warning("`append` should not be `TRUE` when `file` is `NULL`")
+    }
+  } else {
+    filepath <- file
+  }
+
   # Redirect stderr to the desired file.
-  old_fd <- begin_redirect_stderr(file, append)
+  old_fd <- begin_redirect_stderr(filepath, append)
 
   # Evaluate the code.
   eval(expression)
@@ -46,5 +68,13 @@ with_redirect_stderr <- function (expression, file, append = FALSE) {
   # Revert back to the old stderr
   end_redirect_stderr(old_fd)
 
-  invisible(NULL)
+  if (is.null(file)) {
+    # Read the temporary file's contents, delete the file, and return
+    # the contents to the user
+    lines <- readLines(filepath)
+    unlink(filepath)
+    lines
+  } else {
+    invisible(NULL)
+  }
 }
